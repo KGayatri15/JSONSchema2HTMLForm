ce7326877524373333893d54e38e1d429ac9cdc3
KGayatri15 <76037810+KGayatri15@users.noreply.github.com>
2020-12-16 10:53:24 +0530

    Addition of SetData

--- class.js
+++ class.js
@@ -2,46 +2,49 @@ class process{
     static json2HTML(schema){
           if(schema.nodeType === 3){
             var  output = document.createTextNode(schema.textContent);
-            if(schema.childNodes){ process.assignChildNodes(schema.childNodes,output,"set");}
+            if(schema.childNodes){ process.assignChildNodes(schema.childNodes,output);}
           }else{
             var output = document.createElement(schema.tagName);
-            if(schema.attributes){ process.assignAttributes(schema.attributes ,output,"set");}
-            if(schema.childNodes){ process.assignChildNodes(schema.childNodes,output,"set");}
+            if(schema.attributes){ process.assignAttributes(schema.attributes ,output);}
+            if(schema.childNodes){ process.assignChildNodes(schema.childNodes,output);}
           }
         return output;
     }
-    static assignAttributes(obj,objResponse,operation){
+    static assignAttributes(obj,objResponse){
         if (!obj) return;
         if (!objResponse) { var objResponse = {}; };
         for(var key in obj){
-          if(operation === "set")
-            objResponse.setAttribute(key , obj[key]);
-          if(operation === "get" && obj[key].value!== undefined){
-            objResponse[obj[key].name] = obj[key].value;
-          }
+            objResponse = process.setData(obj ,objResponse ,key);
         }
         return objResponse;
     }
-    static assignChildNodes( child,childResponse,operation){
+    static assignChildNodes( child,childResponse){
         if (!child) return;
         if (!childResponse) { var childResponse = []; };
         for(var i = 0;i < child.length;i++){
-          if(operation === "set")
+          if(childResponse instanceof HTMLElement)
             childResponse.appendChild(process.json2HTML(child[i]));
-          if(operation === "get"){
+          else{
             if(child[i].nodeType === Node.TEXT_NODE)
-              childResponse.push(child[i].textContent)
+                childResponse.push(child[i].textContent);
             else
               childResponse.push(process.HTML2json(child[i]));
           }
         }
         return childResponse;
     }
+    static setData(input,output,key){
+      if(typeof output === 'object'&& input[key].value!== undefined)
+          output[input[key].name] = input[key].value;
+      if(output instanceof HTMLElement)
+          output.setAttribute(key,input[key]);
+      return output;
+    }
     static HTML2json(nodeE){
         const value = {
           tagName:nodeE.tagName,
-          attributes:process.assignAttributes(nodeE.attributes,{},"get"),
-          childNodes:process.assignChildNodes(nodeE.childNodes,[],"get"),
+          attributes:process.assignAttributes(nodeE.attributes,{}),
+          childNodes:process.assignChildNodes(nodeE.childNodes,[]),
           nodeType:nodeE.nodeType
         }
         return value;   

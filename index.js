20c4756be7e7ebbf058d4017bc7f1173c0cc8e1d
KGayatri15 <76037810+KGayatri15@users.noreply.github.com>
2020-12-21 11:15:33 +0530

    Delete

--- index.js
+++
@@ -1,7 +0,0 @@
-function start(){
-    document.getElementById("display").appendChild(process.json2HTML(schema));
-}
-function Submit(){
-    alert('Registered Data Successfully !');
-    return true;
-}

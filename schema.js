da42a8cb0e3c5f009772b5a6a571ce10f2aaeff7
KGayatri15 <76037810+KGayatri15@users.noreply.github.com>
2020-12-21 10:26:11 +0530

    Updated Form

--- schema.js
+++ schema.js
@@ -1,66 +1,49 @@
-const schema ={
-    "tagName":"form",
-    "attributes":{ "name":"json" , "method":"POST","onSUbmit":"return Submit();"},
-    "childNodes":[  
-        {
+var form = {
+  "tagName":"form",
+  "name":"json" ,
+  "method":"POST",
+  "onSubmit":"return Submit();",
+  "childNodes":[
+         {
+           "tagName":"label",
+           "innerText":"Username",
+           "for":"Username"
+         },
+         {
+           "tagName":"input",
+           "type":"text",
+           "maxLength":40,
+           "required":"",
+           "name":"Username",
+         },
+         {
           "tagName":"label",
-          "attributes":{"for":"Email"},
-          "childNodes":[{ "nodeType":3, "textContent":"Email" }]
-        }, 
-        {
-            "tagName":"input",
-            "attributes":{ "type":"email","name":"Email","pattern":"[a-z0-9._%+-]+@[a-z0-9.-]+[a-z]{2,4}$","required":""}
+          "innerText":"Email",
+          "for":"Email"
         },
-        {
+         {
+           "tagName":"input",
+           "type":"email",
+           "pattern":"[a-z0-9._%+-]+@[a-z0-9.-]+[a-z]{2,4}$",
+          "required":"",
+           "name":"Email"
+         },
+         {
           "tagName":"label",
-          "attributes":{ "for":"Gender"},
-          "childNodes":[{ "nodeType":3,"textContent":"Gender" }]
+          "innerText":"Password",
+          "for":"Password"
         },
-        {
-         "tagName":"select",
-          "attributes":{"name":"Gender","required":"",},
-          "childNodes":[
-            {
-              "tagName":"option",
-              "attributes":{"value":"Male"},
-              "childNodes":[{ "nodeType":3,"textContent":"Male" }]
-            },
-            {
-              "tagName":"option",
-              "attributes":{ "value":"Female"},
-              "childNodes":[{ "nodeType":3,"textContent":"Female" }]
-            },
-            {
-              "tagName":"option",
-              "attributes":{ "value":"Others"},
-              "childNodes":[{ "nodeType":3,"textContent":"Others" }]
-            }
-          ]
-        },
-        {
-          "tagName":"label",
-          "attributes":{  "for":"MagicNo"},
-          "childNodes":[{  "nodeType":3,  "textContent":"Magic Number" }]
-        },
-        {
-          "tagName":"input",
-          "attributes":{"type":"number", "name":"MagicNo", "value":"1","required":"","min":1}
-        },
-        {
-          "tagName":"label",
-          "attributes":{ "for":"Password"},
-          "childNodes":[{ "nodeType":3,"textContent":"Password" }]
-        },
-        {
-          "tagName":"input",
-          "attributes":{ "type":"password", "name":"Password","minLength":6,"required":""} 
-        },
-        {
-          "tagName":"input",
-          "attributes":{ "type":"submit","value":"Submit","name":"Submit"}
+         {
+           "tagName":"input",
+           "type":"password",
+           "minLength":6,
+           "required":"",
+           "name":"Password"
+         },
+         {
+             "tagName":"input",
+              "type":"submit",
+              "value":"submit",
         }
-      ]
-}
-
-
-
+   ]
+ }
